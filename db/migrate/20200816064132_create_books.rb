1eeec8425ef08ee0bba2e6fb85e2314bd7c8c330
vagrant <vagrant@localhost.localdomain>
2020-08-16 06:58:28 +0000

    ２回めâののテスト

--- db/migrate/20200816064132_create_books.rb
+++ db/migrate/20200816064132_create_books.rb
@@ -2,7 +2,7 @@ class CreateBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :books do |t|
       t.string :title
-      t.texr :body
+      t.text :body
 
       t.timestamps
     end
